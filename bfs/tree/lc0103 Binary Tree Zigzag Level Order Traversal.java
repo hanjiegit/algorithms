8ee4d7f0e0a51b36911dc9a9358bc74c2c5f2419
hanjiegit <53994235+hanjiegit@users.noreply.github.com>
2020-05-20 16:28:51 +0800

    Update lc0103 Binary Tree Zigzag Level Order Traversal.java
    按照模板进行了一些修改，原来的代码中，压入next链表的左右节点前后顺序需要根据奇偶行调整，容易出错。直接修改成为模板中的形式。这样不容易出错。
    PS：这么写效率还提高了！这是为什么？？？感觉没有提升效率的动作啊

--- bfs/tree/lc0103 Binary Tree Zigzag Level Order Traversal.java
+++ bfs/tree/lc0103 Binary Tree Zigzag Level Order Traversal.java
@@ -1,4 +1,4 @@
-// 1ms 71.53%
+// 1ms 97.76%
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         ArrayList<List<Integer>> results = new ArrayList();
@@ -8,21 +8,33 @@ class Solution {
         boolean odd = true;
         while(!layer.isEmpty()){
             LinkedList next = new LinkedList();
-            ArrayList<Integer> arr = new ArrayList();
-            while(!layer.isEmpty()){
-                if(odd){
-                    TreeNode n = layer.pollFirst();
-                    if(n.left != null) next.offerLast(n.left);
-                    if(n.right != null) next.offerLast(n.right);
-                    arr.add(n.val);
-                }
-                else{
-                    TreeNode n = layer.pollLast();
-                    if(n.right != null) next.offerFirst(n.right);
-                    if(n.left != null) next.offerFirst(n.left);
-                    arr.add(n.val);
-                }
-            }
+//            ArrayList<Integer> arr = new ArrayList();
+//             while(!layer.isEmpty()){
+//                 if(odd){
+//                     TreeNode n = layer.pollFirst();
+//                     if(n.left != null) next.offerLast(n.left);
+//                     if(n.right != null) next.offerLast(n.right);
+//                     arr.add(n.val);
+//                 }
+//                 else{
+//                     TreeNode n = layer.pollLast();
+//                     if(n.right != null) next.offerFirst(n.right);
+//                     if(n.left != null) next.offerFirst(n.left);
+//                     arr.add(n.val);
+//                 }
+            //此处按照模板格式进行套写，因为只需要将结果数组中的数字进行reverse，所以还可以采用for(TreeNode n: layer）的方式进行遍历
+            //这样避免了next中添加元素时候，正反两项需要注意放入n.left与n.right顺序的问题，不过这里需要将arr设置成LinkedList
+               LinkedList<Integer> arr = new LinkedList();
+               for(TreeNode n: layer){
+                   if(odd){
+                       arr.offerLast(n.val);        //奇数行就放在最后
+                   }else{
+                       arr.offerFirst(n.val);       //偶数行就放在前面，顺序就是倒置了
+                   }
+                   //将节点的子节点依次放入next链表中
+                   if(n.left!=null) next.offerLast(n.left);
+                   if(n.right!=null) next.offerLast(n.right);
+               }
             odd = !odd;
             results.add(arr);
             layer = next;
